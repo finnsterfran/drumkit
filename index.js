ce7c4a90b1385e63e9f97de05d7dd01c83aa7551
Snoopy Nerd <71873797+finnsterfran@users.noreply.github.com>
2021-07-03 12:00:17 +0200

    fix sound links

--- index.js
+++ index.js
@@ -40,22 +40,22 @@ function makeSound(key) {
             break;
 
         case "d":
-            var tom1 = new Audio("/sounds/tom-1.mp3");
+            var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
 
         case "j":
-            var tom2 = new Audio("/sounds/tom-2.mp3");
+            var tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
             break;
 
         case "k":
-            var tom3 = new Audio("/sounds/tom-3.mp3");
+            var tom3 = new Audio("sounds/tom-3.mp3");
             tom3.play();
             break;
 
         case "l":
-            var tom4 = new Audio("/sounds/tom-4.mp3");
+            var tom4 = new Audio("sounds/tom-4.mp3");
             tom4.play();
             break;
 
